5decf928e0182edeed6ca1c0751b706e841202d5
Thales Nunes <thales.o.nunes@gmail.com>
2022-08-02 09:51:06 -0300

    Criando o POST para Book e salvando no Banco

--- mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtesionFunction.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtesionFunction.kt
@@ -1,8 +1,12 @@
 package com.mercadolivro.extension
 
+import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.emums.BookStatus
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
+import java.awt.print.Book
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
     return CustomerModel(name = this.name, email = this.email)
@@ -10,4 +14,13 @@ fun PostCustomerRequest.toCustomerModel(): CustomerModel {
 
 fun PutCustomerRequest.toCustomerModel(id: Int): CustomerModel {
     return CustomerModel(id = id, name = this.name, email = this.email)
+}
+
+fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel{
+    return BookModel(
+        name = this.name,
+        price = this.price,
+        status = BookStatus.ATIVO,
+        customer = customer
+    )
 }
